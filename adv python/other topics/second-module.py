a5cebd8006e78d9d02bc6bd9ffeb0001351b1411
alaikasadana <alaikasadana@gmail.com>
2025-11-22 15:32:49 +0530

    python

---
+++ adv python/other topics/second-module.py
@@ -0,0 +1,24 @@
+# import firstmodule 
+
+# print(firstmodule.x)
+# print(firstmodule.y)
+# print(firstmodule.z)
+
+# p=firstmodule.x
+# q=firstmodule.y
+# firstmodule.add(p,q)
+
+
+#====================================================================================================
+
+# from firstmodule import x,y
+
+# print(x)
+# print(y)
+
+#====================================================================================================
+
+# from firstmodule import x as xyz
+
+# print(xyz)
+
